471520e9c090652bb70561905db906370e98c3d3
Padmagirish-apsi <padmagirish@apsilabs.com>
2024-09-23 12:42:33 +0530

    Add project files.

---
+++ BulkyWeb/Models/Category.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BulkyWeb.Models
+{
+    // DB sechema
+    public class Category
+    {
+        [Key]// constrants for database for eg: Primary Key
+        public int Id { set; get;}
+        [Required]
+        public string Name {set; get;}
+        public int DisplayOrder { set; get; }
+
+    }
+}
