71773e4c2ecaa4074aef32c4ac2ca83d180ea240
harish <harishkumar@apsilabs>
2024-09-27 14:48:42 +0530

    repository patten created

---
+++ BulkyWeb/Repository/IRepository/IRepository.cs
@@ -0,0 +1,15 @@
+﻿using System.Linq.Expressions;
+
+namespace BulkyWeb.Repository.IRepository
+{
+    public interface IRepository<T> where T : class
+    {
+        IEnumerable<T> GetAll();
+        T Get(Expression<Func<T, bool>> filter);
+        void Add(T Entity);
+        void Remove(T Entity);
+        void RemoveRang(IEnumerable<T> Entity);
+
+
+    }
+}
---
+++ BulkyWeb/Repository/Repesitory.cs
@@ -0,0 +1,38 @@
+﻿using BulkyWeb.Data;
+using BulkyWeb.Models;
+using BulkyWeb.Repository.IRepository;
+using Microsoft.EntityFrameworkCore;
+using System.Collections;
+using System.Linq.Expressions;
+
+namespace BulkyWeb.Repository
+{
+    public class Repesitory<T> : IRepository<T> where T : class
+    {
+        private readonly ApplicationDbContext _db;
+        internal DbSet<T> dbSet;
+        public Repesitory(ApplicationDbContext db) {
+            _db = db;
+            dbSet = _db.Set<T>();
+        }
+        public IEnumerable GetAll()
+        {
+            
+        }
+        public T Get(Expression<Func<T, bool>> Filter)
+        {
+
+        }
+        public void Add(T Entity)
+        {
+            dbSet.Add(Entity);
+        }
+        public void Remove(T Entity)
+        { 
+        }
+        public void RemoveRange(IEnumerable<T> Entity)
+        {
+
+        }
+    }
+}
