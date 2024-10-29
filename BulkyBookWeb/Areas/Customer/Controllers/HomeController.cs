2e4a0d5123b2b29c7ac2c98544b8b20630fd37bd
harish <harishkumar@apsilabs>
2024-10-29 14:03:05 +0530

    Authentication is created

---
+++ BulkyBookWeb/Areas/Customer/Controllers/HomeController.cs
@@ -0,0 +1,40 @@
+﻿using BulkyBookWeb.Models;
+using BulkyBookWeb.Repository.IRepository;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace BulkyBookWeb.Areas.Customer.Controllers { 
+
+    [Area("Customer")]
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        internal readonly IUnitOfWork _unitOfWork;
+        public HomeController(ILogger<HomeController> logger, IUnitOfWork unitOfWork)
+        {
+            _logger = logger;
+            _unitOfWork = unitOfWork;
+        }
+        public IActionResult Index()
+        {
+            IEnumerable<Product> productObj = _unitOfWork.Product.GetAll(includeProperty:"Category");
+            return View(productObj);
+        }
+        public IActionResult Details(int ?ProductId)
+        {
+            Product productList = _unitOfWork.Product.Get(item => item.Id == ProductId, includeProperty:"Category");
+            return View(productList);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ BulkyBookWeb/Program.cs
@@ -0,0 +1,41 @@
+using BulkyBookWeb.Data;
+using BulkyBookWeb.Repository;
+using BulkyBookWeb.Repository.IRepository;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+// Add services to the container.
+
+builder.Services.AddControllersWithViews();
+var connectionString = builder.Configuration.GetConnectionString("DefaultString");// to get connection string
+IServiceCollection serviceCollection = builder.Services.AddDbContext<ApplicationDbContext>(options=>options.UseMySql(connectionString,new MySqlServerVersion(new Version(6,0,0))));
+builder.Services.AddScoped<IUnitOfWork,UnitOfWork>();
+
+builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+    .AddEntityFrameworkStores<ApplicationDbContext>();
+
+var app = builder.Build();
+    
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+app.UseAuthentication(); ;
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{area=Customer}/{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
